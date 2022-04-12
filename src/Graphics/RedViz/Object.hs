3058c21fa4d6e7aeb5bfa879948aef24d89dffd2
madjestic <madjestic13@gmail.com>
2022-04-12 16:28:24 +0200

    adding initial rotation value to Object

--- src/Graphics/RedViz/Object.hs
+++ src/Graphics/RedViz/Object.hs
@@ -7,12 +7,15 @@ module Graphics.RedViz.Object
   , programs
   , transforms
   , time
+  , ypr0
+  , ypr
   , defaultObject'
   ) where
 
 import Control.Lens hiding (transform, pre)
 import Graphics.Rendering.OpenGL (Program)
 import Linear.Matrix (M44)
+import Linear.V3
 
 import Graphics.RedViz.Descriptor
 import Graphics.RedViz.Material
@@ -26,15 +29,14 @@ data Object'
       , _materials   :: [Material]   -- | hence [Material] is present on the Object level too, we use that value, instead of looking it up from respective VGeo.
       , _programs    :: [Program]    -- | Shader Programs
       , _transforms  :: ![M44 Double]
+      , _ypr0        :: V3 Double
+      , _ypr         :: !(V3 Double)
       , _time        :: Double
      } deriving Show
 $(makeLenses ''Object')
 
+zeroV3 :: V3 Double
+zeroV3 = V3 0 0 0
+
 defaultObject' :: Object'
-defaultObject' =
-  Object'
-   []
-   []
-   []
-   []
-   0.0
+defaultObject' = Object' [] [] [] [] zeroV3 zeroV3 0.0
